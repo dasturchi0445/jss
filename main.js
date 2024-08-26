cdebc5a0bc6a84019decea0afbc67140a85322c2
ismoilnuriddinov <umidafayziyeva89@gmail.com>
2024-08-26 11:35:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let ism = document.getElementById("ism");
+let familiya = document.getElementById("familiya");
+let yosh = document.getElementById("yosh")
+let ismrazmer = document.getElementById("ismrazmer");
+
+let ismValue = prompt("Ismingizni kiriting...");
+let familiyaValue = prompt("Familiyangizni kiriting...");
+let yoshValue = prompt("yoshingizni kiriting")
+let razmerValue = prompt("ismingizni katta harf yoki kichikligini tanlang  1)katta 2)kichik");
+
+ism.textContent = ismValue;
+familiya.textContent = familiyaValue;
+yosh.textContent = yoshValue;
+ismrazmer.textContent = razmerValue
+
